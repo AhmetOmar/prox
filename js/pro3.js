9ebb0d1c089092f1f83343ec2b616223c9d962a1
AhmetOmar <ahmed.omar82233@gmail.com>
2021-05-09 12:46:13 +0200

    Adding Some Code Files

---
+++ js/pro3.js
@@ -0,0 +1,18 @@
+var btntest=document.getElementById("btntest");
+var divtest=document.getElementById("divtest");
+var tbody=document.body;
+
+btntest.addEventListener('click',function(){
+    
+})
+
+
+
+
+
+
+
+
+
+
+
